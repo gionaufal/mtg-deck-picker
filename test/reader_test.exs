4c47e60632d91dcda8c873247cedf9860e91d075
Giovanni Naufal <giovanni.naufal@locaweb.com.br>
2018-02-27 18:50:59 -0300

    Add reader test

---
+++ test/reader_test.exs
@@ -0,0 +1,13 @@
+defmodule ReaderTest do
+  use ExUnit.Case
+  doctest Reader
+
+  test "reads list of cards" do
+    assert is_list(Reader.read_cards)
+  end
+
+  test "list has cards and counts" do
+    card = Reader.read_cards |> Enum.at(0)
+    assert card == %{card: "Ancient Crab", count: 2}
+  end
+end
